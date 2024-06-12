5d02212a402139fb74e54ff12c417a320ab8ae4e
Apeehal <appeehal@hotmail.com>
2024-06-12 18:25:27 +0100

    Considering change in solubility

--- final_script.py
+++ final_script.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 interfacial_energy = 475*10**-3
-equilibreum_concentration = 0.02
+equilibreum_concentration = 0.011
 R = 8.314
-t = np.linspace(0,30*60,30*60)
+t = np.linspace(0,30*60,60*60)
 
 #interfacial_energy = [51*10^-3, 123*10^-3, 314*10^-3]
 
@@ -24,6 +24,7 @@ plt.grid(True)
 plt.show()
 
 interfacial_energy = 450*10**-3
+equilibreum_concentration = 0.003
 diffusion_coefficient = 4.0*10**-18
 T = 660+273.15
 molar_volume = (7.09 * 10**-6) * 3 * 16.35 * (T-25)
@@ -41,11 +42,12 @@ plt.show()
 
 
 interfacial_energy = 350*10**-3
+equilibreum_concentration = 0.0005
 diffusion_coefficient = 2.0*10**-21
 T = 500+273.15
 molar_volume = (7.09 * 10**-6) * 3 * 16.35 * (T-25)
 r3 = np.cbrt((8*interfacial_energy*(molar_volume**2)*(equilibreum_concentration)*(diffusion_coefficient)*(t))/(9*R*T) + ((r2[-1])**3))
-
+print(r3[-1])
 
 # Plotting the graph
 plt.plot(t, r3, label='Sample V Radius')
