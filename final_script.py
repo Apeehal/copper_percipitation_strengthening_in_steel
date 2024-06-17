b245213328a34f772cb451bd170e6cef479e883e
Apeehal <appeehal@hotmail.com>
2024-06-17 16:07:28 +0100

    Starting to look accuate. Need to check k-value and units

--- final_script.py
+++ final_script.py
@@ -1,33 +1,81 @@
-
 import numpy as np
-from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
+# Define constants and initial conditions
+k = 10000000000000000000000000000000
+cu_wt = 1.04 / 100
+
+fe_density = 7800 / (1e-27)
+mol_cu = 63.546 * 1e-3
+cu_density = 8850 / (1e-27)
+a = 348.79
+e = np.exp(1)
 pi = np.pi
-e = np.e
-# Constants
-R = 8.314  # Gas constant in J/(mol*K)
-t = np.linspace(0.00, 30*60, 100000)  # Time array from 0 to 30 minutes with 1-second intervals
+
+# Time parameters
+t = np.linspace(0, 1, 10000)
+dt = t[1] - t[0]  # Timestep based on the linspace definition
 
 # Initial conditions
-interfacial_energy = [475e-3, 450e-3, 350e-3]  # in J/m^2
-#interfacial_energy = [51*10**-3, 123*10**-3, 314*10**-3]  # in J/m^2
-#equilibreum_concentration = [0.011, 0.003, 0.0005]  # in mol/m^3
-diffusion_coefficient = [2.6e-16, 4.0e-18, 2.0e-21]  # in m^2/s
-T = [780 + 273.15, 660 + 273.15, 500 + 273.15]  # in K
-molar_volume = [(7.09e-6) * 3 * 16.35 * (T[i] - 25) for i in range(len(T))]
+v0 = (0.128 * 10) ** 3 * (4 / 3)
+v = np.zeros_like(t)
+v[0] = v0
 
-cu_wt = 1.04/100
-fe_density = 7800
-mol_cu = 63.546*10**-3
-cu_density = 8850
-#a = 1000*10**-9
-a = 348.79*10**-9
+# Precompute some constants
+const1 = (k * cu_wt * fe_density) / mol_cu
+const2 = (4 * pi)
+const3 = (k * cu_density) / ((mol_cu) * (a ** 3))
+const4 = e ** (np.cbrt((4 * pi) / 3))
 
-k = 1
-dvdt = []
-v = np.zeros(100000)
-v[0] =( (0.128*10**(-9)) ** (3) ) * (4/3)
- 
-for i in range(1, 100000):
-    dvdt[i] = ((k*cu_wt*fe_density)/(mol_cu))  /  ( ( (  (3*e **(np.cbrt(((4*pi)/v[i])))) - v[i]*e**( (-v[i]/3) + (np.cbrt((4*pi)/3)) ) )    / ( 4* pi  ) )    + (   ((k*cu_density*t)+(k*cu_density*v[i]))/((mol_cu)*(a**3))  )       + (  ((v[-i]) * (e**(-v[i]/3)) * (e**(np.cbrt((4*pi)/3))) )/(4*pi)   )    ) 
+# Iterate to solve for v and dvdt
+dvdt = np.zeros_like(t)
+
+for i in range(1, len(t)):
+    try:
+        term1 = const1
+
+        term2_numerator = (3 * e ** (np.cbrt((4 * pi) / v[i-1]))) - v[i-1] * e ** ((-v[i-1] / 3) + np.cbrt((4 * pi) / 3))
+        term2 = term2_numerator / const2
+
+        term3 = (const3 * t[i]) + (const3 * v[i-1])
+
+        if i > 0:
+            term4 = (v[i-1] * e ** (-v[i] / 3)) * const4 / const2
+        else:
+            term4 = (v[i] * e ** (-v[i] / 3)) * const4 / const2 # This case is handled implicitly by starting from i=1
+
+        dvdt[i] = term1 / (term2 + term3 + term4)
+
+        # Update v using the computed dvdt
+        v[i] = v[i-1] + dvdt[i] * dt
+
+    except ZeroDivisionError as zde:
+        print(f"ZeroDivisionError at index {i}: {zde}")
+        dvdt[i] = np.nan  # Handle division by zero
+    except Exception as ex:
+        print(f"Error at index {i}: {ex}")
+        dvdt[i] = np.nan  # Handle other exceptions
+
+# Plot the results
+plt.figure(figsize=(10, 6))
+plt.plot(t, dvdt, label='dvdt', marker='o')
+plt.xlabel('Time t')
+plt.ylabel('dvdt')
+plt.title('Plot of dvdt over Time')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+r = np.cbrt((3/(4*pi))*(v))
+
+# Plot v over time as well
+plt.figure(figsize=(10, 6))
+plt.plot(t, r, label='v', marker='o')
+plt.xlabel('Time t')
+plt.ylabel('v')
+plt.title('Plot of v over Time')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+print(r)
