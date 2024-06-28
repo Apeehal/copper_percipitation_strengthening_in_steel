5a4875e7bc07a942a96f31e8e455177c862ac36c
Apeehal <appeehal@hotmail.com>
2024-06-28 16:18:31 +0100

    Change in strength modelled

--- final_script.py
+++ final_script.py
@@ -140,18 +140,85 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
-vol_frac = 9.11e-3
-
+"""
+OROWAN MODEL 
+- Source: https://www.sciencedirect.com/science/article/pii/S0927025614002572
+- Fitted the inputs to match the target value of 78MPa for tensile strength
+- Target: 78MPa
+"""
+#vol_frac = 9.11e-3
+vol_frac = 0.012293
 #shear stress orowan:
-J = 1 #assume 0.8 for now (can be used as a fitting parameter, meant to be betwee 0.8 and 1)
+J = 0.8 #assume 0.8 for now (can be used as a fitting parameter, meant to be betwee 0.8 and 1)
 G = 48300e6
 b = 0.255e-9
-nu = 0.33 #can again be used as a fiting parameter (between 0.25 and 0.33)
-Ls = np.sqrt(r3[-1]*((2*pi)/(3*vol_frac)))
+nu = 0.25 #can again be used as a fiting parameter (between 0.25 and 0.33)
+Ls = r3[-1]*(np.sqrt(((2*pi)/(3*vol_frac))))
+
+gain_tensile_strength_orowan = (((G*b)/Ls) * J * 2)*10**-6
+print("Orowan", gain_tensile_strength_orowan) 
+
+
+
+"""
+ASHBY-OROWAN MODEL 
+- Source: https://www.sciencedirect.com/science/article/pii/S0927025614002572
+- Fitted the inputs to match the target value of 78MPa for tensile strength
+- Target: 78MPa
+- Assumed Tensile Strength = 2 * Shear Strength (generally true, but the Taylor Factor for polycrystalline materials is 3)
+"""
+
+#vol_frac = 9.11e-3
+vol_frac = 0.012293
+#shear stress orowan:
+J = 0.8 #assume 0.8 for now (can be used as a fitting parameter, meant to be betwee 0.8 and 1)
+G = 48300e6
+b = 0.255e-9
+nu = 0.25 #can again be used as a fiting parameter (between 0.25 and 0.33)
+Ls = r3[-1]*(np.sqrt(((2*pi)/(3*vol_frac))))
 rs = r3[-1]
 ri = 4*b #meant to be between b and 4b 
 
-gain_tensile_strength = 3*((J*G*b)/(2*pi*np.sqrt(1-nu)*Ls))*np.log(2*rs/ri)
-print(gain_tensile_strength)
+gain_tensile_strength_Ashby_Orowan = (2*((J*G*b)/(2*pi*np.sqrt(1-nu)*Ls))*np.log(2*rs/ri) ) *10**-6
+print("Ashby-Orowan", gain_tensile_strength_Ashby_Orowan)
 
 
+
+
+
+
+
+"""
+JACKSON-REED MODEL 
+- Source: https://www.sciencedirect.com/science/article/pii/S2589152920300995
+- Fitted the inputs to match the target value of 78MPa for tensile strength
+- Target: 78MPa
+- Assumed Tensile Strength = 2 * Shear Strength (generally true, but the Taylor Factor for polycrystalline materials is 3)
+"""
+
+#vol_frac = 9.11e-3
+vol_frac = 0.012293
+#shear stress orowan:
+J = 0.8 #assume 0.8 for now (can be used as a fitting parameter, meant to be betwee 0.8 and 1)
+G = 48300e6
+b = 0.255e-9
+M = 2
+gain_tensile_strength_Jackson_Reed = (((M*G*b)/(r3[-1]))*np.sqrt((1.5*vol_frac)) * ((J)/(pi**(3/2))) * np.sqrt(((2*pi*interfacial_energy[2]*r3[-1])/(J*G*b*b))-1))*10**-6
+print("Jackson-Reed", gain_tensile_strength_Jackson_Reed)
+
+
+
+"""
+Russel-Brown Model 
+- Source: https://www.mdpi.com/2075-4701/10/10/1350
+- For Yield Stress
+- Target: 15MPa
+"""
+
+vol_frac = 0.012293
+G = 48300e6
+b = 0.255e-9
+L = (1.77*r3[-1])/(np.sqrt(vol_frac))
+
+gain_tensile_strength_Russel_Brown = ((0.8*G*b)/(L))*(np.sqrt(1-(0.6**2)))*10**-6
+print("Russel-Brown", gain_tensile_strength_Russel_Brown)
